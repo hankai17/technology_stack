a7bdfd388c3689b26bb9a6611ffe6b572d40a9d8
hankai17 <867614535@qq.com>
2024-03-08 02:16:03 -0500

    add os_thread1 for more func

--- linux/day09/3mult_thread_concurrent/os_threads1.c
+++ linux/day09/3mult_thread_concurrent/os_threads1.c
@@ -9,10 +9,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
+#include <signal.h>
 #include <sys/socket.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 #define MAXLINE 8192
-#define SERV_PORT 91
+#define SERV_PORT 90
 
 struct s_info {                     //定义一个结构体, 将地址结构跟cfd捆绑
     struct sockaddr_in cliaddr;
@@ -33,8 +38,8 @@ again:
         if ((n = accept(fd, sa, salenptr)) < 0) {
                 if ((errno == ECONNABORTED) || (errno == EINTR))
                         goto again;
-                else
-                        perr_exit("accept error");
+                //else
+                //        perr_exit("accept error");
         }
         return n;
 }
@@ -215,52 +220,107 @@ int file_size(const char* filePath) {
         return sb.st_size;
 }
 
-void *do_file(void *arg)
+void *do_work_file(void *arg)
 {
     int n,i;
     struct s_info *ts = (struct s_info*)arg;
+    char buf[MAXLINE];
     char str[INET_ADDRSTRLEN];
-    const char *file = "/usr/local/nginx/html/2.exe";
-    size_t size = file_size(file);
-    if (size <= 0) {
-      printf("file size 0: %s\n", file);
-      return (void *)0;
-    }
-    char resp_1024[1024] = {0};
-    sprintf(resp_1024,
-        "HTTP/1.1 200 OK\r\nX-Content-Length: 1024000\r\nContent-Range: bytes 0-1023/10240000\r\nContent-Length-private: 3\r\nContent-Length: %d\r\n\r\n",
-        size);
-	char tmp_buf[1024*64]={0};
-    Read(ts->connfd, tmp_buf, MAXLINE);
 
+    memset(buf, 0, sizeof(buf));
+    n = Read(ts->connfd, buf, MAXLINE);                     //读客户端
+    if (n == 0) 
+    {
+        printf("the client %d closed...\n", ts->connfd);
+        goto end;
+    }
     printf("received from %s at PORT %d\n",
             inet_ntop(AF_INET, &(*ts).cliaddr.sin_addr, str, sizeof(str)),
             ntohs((*ts).cliaddr.sin_port));                 //打印客户端信息(IP/PORT)
-    Write(ts->connfd, resp_1024, strlen(resp_1024));
 
-	FILE *fin = fopen(file,"r");
-	if(!fin) return (void*)0;
-	char  buf[1024*64]={0};
-	int  blockSize = sizeof(buf);
+    printf("%s\n", buf);
+
+    ///  open file & send it
+    const char *filename = "1.log";
+    int size = file_size(filename);
+    if (size == 0) {
+        goto end;
+    }
+
+    char resp_1024[1024] = {0};
+    sprintf(resp_1024, "HTTP/1.1 200 OK\r\nContent-Length: %d\r\n\r\n", size);
+    Write(ts->connfd, resp_1024, strlen(resp_1024));                              //回写给客户端
+
+    printf("%s\n", resp_1024);
+
+	FILE *fin = fopen(filename, "r");
+	if (!fin) {
+        goto end;
+    }
+
+	char fbuf[1024*4]={0};
+	int blockSize = sizeof(fbuf);
 	int writelen = 0;
-
-	while( size >0 )
+    i = 0;
+	while (size > 0)
 	{
-		if(size< blockSize)
+		if (size < blockSize)
 			blockSize = size;
 
-		fread( buf, sizeof(char), blockSize, fin );
-        writelen = Write(ts->connfd, buf, blockSize);                              //回写给客户端
+		int t = fread(fbuf, sizeof(char), blockSize, fin);
+        writelen = Write(ts->connfd, fbuf, blockSize);                              //回写给客户端
 		size = size - blockSize;
+        i++;
+        if (i == 10) {
+            //break;
+        }
 		if(writelen != blockSize)
 		{
-			fclose(fin);
-            Close(ts->connfd);
-            return (void *)0;
+            goto end;
 		}
 	}
-	fclose(fin);
+
+end:
+    if (fin) {
+	    fclose(fin);
+    }
+    sleep(1);
     Close(ts->connfd);
+    free(ts);
+    return (void *)0;
+}
+
+void *do_post(void *arg)
+{
+    int n,i;
+    struct s_info *ts = (struct s_info*)arg;
+    char buf[MAXLINE];
+    char str[INET_ADDRSTRLEN];
+    char resp_1024[1024] = "HTTP/1.1 200 OK\r\nSet-Cookie:k1=v1\r\nContent-Length: 12\r\n\r\nabcdefghijkldef";
+
+    if (1) 
+    {
+        memset(buf, 0, sizeof(buf));
+        for (i = 0; i < 10; i++) {
+            n = Read(ts->connfd, buf, MAXLINE);                     //读客户端
+            if (n == 0) 
+            {
+                printf("the client %d closed...\n", ts->connfd);
+                break;                                              //跳出循环,关闭cfd
+            }
+        }
+        printf("received from %s at PORT %d\n",
+                inet_ntop(AF_INET, &(*ts).cliaddr.sin_addr, str, sizeof(str)),
+                ntohs((*ts).cliaddr.sin_port));                 //打印客户端信息(IP/PORT)
+
+        printf("%s\n", buf);
+        Write(ts->connfd, resp_1024, strlen(resp_1024));                              //回写给客户端
+        printf("%s\n", resp_1024);
+
+    }
+    Close(ts->connfd);
+
+    free(ts);
     return (void *)0;
 }
 
@@ -270,10 +330,11 @@ void *do_work(void *arg)
     struct s_info *ts = (struct s_info*)arg;
     char buf[MAXLINE];
     char str[INET_ADDRSTRLEN];
-    char resp_1024[1024] = "HTTP/1.1 200 OK\r\nX-Content-Length: 1024000\r\nContent-Range: bytes 0-1023/10240000\r\nContent-Length-private: 3\r\nContent-Length: 10\r\n\r\nhelloworld";
+    char resp_1024[1024] = "HTTP/1.1 200 OK\r\nSet-Cookie:k1=v1\r\nContent-Length: 15\r\nConnection: Keep-Alive\r\n\r\nabcdefghijkldef";
 
     while (1) 
     {
+        memset(buf, 0, sizeof(buf));
         n = Read(ts->connfd, buf, MAXLINE);                     //读客户端
         if (n == 0) 
         {
@@ -284,25 +345,25 @@ void *do_work(void *arg)
                 inet_ntop(AF_INET, &(*ts).cliaddr.sin_addr, str, sizeof(str)),
                 ntohs((*ts).cliaddr.sin_port));                 //打印客户端信息(IP/PORT)
 
-        if(strstr(buf, "siteinfo.xml") != NULL) {
-                sleep(15);
-        }
+        printf("%s\n", buf);
         Write(ts->connfd, resp_1024, strlen(resp_1024));                              //回写给客户端
+        printf("%s\n", resp_1024);
 
         break;
     }
     Close(ts->connfd);
 
+    free(ts);
     return (void *)0;
 }
 
 int main(void)
 {
+    signal(SIGPIPE, SIG_IGN);
     struct sockaddr_in servaddr, cliaddr;
     socklen_t cliaddr_len;
     int listenfd, connfd;
     pthread_t tid;
-    struct s_info ts[256];      //根据最大线程数创建结构体数组.
     int i = 0;
 
     listenfd = Socket(AF_INET, SOCK_STREAM, 0);
@@ -313,8 +374,7 @@ int main(void)
     servaddr.sin_port = htons(SERV_PORT);
 
     int opt = 1;
-    // sockfd为需要端口复用的套接字
-    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&opt, sizeof(opt));
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     Bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
@@ -325,11 +385,14 @@ int main(void)
     {
         cliaddr_len = sizeof(cliaddr);
         connfd = Accept(listenfd, (struct sockaddr *)&cliaddr, &cliaddr_len);
-        ts[i].cliaddr = cliaddr;
-        ts[i].connfd = connfd;
+        printf("accept fd: %d\n", connfd);
+        struct s_info* ts = (struct s_info*)malloc(sizeof(struct s_info));
+        ts->cliaddr = cliaddr;
+        ts->connfd = connfd;
         /* 达到线程最大数时，pthread_create出错处理, 增加服务器稳定性 */
-        //pthread_create(&tid, NULL, do_file, (void*)&ts[i]);
-        pthread_create(&tid, NULL, do_work, (void*)&ts[i]);
+        pthread_create(&tid, NULL, do_work, (void*)ts);
+        //pthread_create(&tid, NULL, do_post, (void*)&ts);
+        //pthread_create(&tid, NULL, do_work_file, (void*)ts);
         pthread_detach(tid);
         i++;
     }
