f94c952cdda72c9886e6c0a58c7c2bf77345099d
hankai17 <hankai17@126.com>
2026-02-05 22:16:27 +0800

    softmax gad

--- dl/day02/2.1softmax_regession_scratch.py
+++ dl/day02/2.1softmax_regession_scratch.py
@@ -4,6 +4,7 @@ from torch.utils import data
 from torchvision import transforms
 from d2l import torch as d2l
 import matplotlib.pyplot as plt
+from IPython import display
 
 class Accumulator:  #@save
     """在n个变量上累加"""
@@ -18,6 +19,46 @@ class Accumulator:  #@save
 
     def __getitem__(self, idx):
         return self.data[idx]
+
+class Animator:  #@save
+    """在动画中绘制数据"""
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(3.5, 2.5)):
+        # 增量地绘制多条线
+        if legend is None:
+            legend = []
+        d2l.use_svg_display()
+        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        # 使用lambda函数捕获参数
+        self.config_axes = lambda: d2l.set_axes(
+            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        # 向图表中添加多个数据点
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        display.display(self.fig)
+        display.clear_output(wait=True)
 
 def softmax(X):
     X_exp = torch.exp(X)
@@ -57,6 +98,42 @@ def evaluate_accuracy(net, data_iter):  #@save
             metric.add(accuracy(net(X), y), y.numel())
     return metric[0] / metric[1]
 
+def train_epoch_ch3(net, train_iter, loss, updater):  #@save
+    """训练模型一个迭代周期（定义见第3章）"""
+    if isinstance(net, torch.nn.Module):                # 将模型设置为训练模式
+        net.train()
+    metric = Accumulator(3)                             # 训练损失总和、训练准确度总和、样本数
+    for X, y in train_iter:
+        y_hat = net(X)                                  # 计算梯度并更新参数
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):  # 使用PyTorch内置的优化器和损失函数
+            updater.zero_grad()
+            l.mean().backward()
+            updater.step()                              # 更新W b 
+        else:                                           # 使用定制的优化器和损失函数
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+    return metric[0] / metric[2], metric[1] / metric[2] # 返回训练损失 和 训练精度
+
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):  #@save
+    """训练模型（定义见第3章）"""
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],
+                        legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    train_loss, train_acc = train_metrics
+    assert train_loss < 0.5, train_loss
+    assert train_acc <= 1 and train_acc > 0.7, train_acc
+    assert test_acc <= 1 and test_acc > 0.7, test_acc
+
+lr = 0.1
+
+def updater(batch_size):
+    return d2l.sgd([W, b], lr, batch_size)
+
 batch_size = 256
 train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
 #for X, y in train_iter:
@@ -82,7 +159,22 @@ X = torch.normal(0, 1, (2, 5))
 #print(cross_entropy(y_hat, y))                              # 实现交叉熵损失函数  [2.3, 0.7]
 #print(accuracy(y_hat, y) / len(y))
 
-print(evaluate_accuracy(net, test_iter))
+#print(evaluate_accuracy(net, test_iter))
+
+num_epochs = 10
+train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
 
 
+def predict_ch3(net, test_iter, n=6):  #@save
+    """预测标签（定义见第3章）"""
+    for X, y in test_iter:
+        break
+    trues = d2l.get_fashion_mnist_labels(y)
+    preds = d2l.get_fashion_mnist_labels(net(X).argmax(axis=1))
+    titles = [true +'\n' + pred for true, pred in zip(trues, preds)]
+    d2l.show_images(
+        X[0:n].reshape((n, 28, 28)), 1, n, titles=titles[0:n])
 
+predict_ch3(net, test_iter)
+
+plt.show()
